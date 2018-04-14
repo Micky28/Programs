dfd06d4493be40b309373081e0fb70897ef54792
Mahesh M R <mahesh.mr2012@gmail.com>
2018-04-15 00:22:53 +0530

    Programs

---
+++ Programs/src/com/mr/collections/SampleArrayList.java
@@ -0,0 +1,11 @@
+package com.mr.collections;
+
+import java.util.ArrayList;
+
+public class SampleArrayList {
+	public static void main(String[] args) {
+		ArrayList a = new ArrayList();
+		a.add("Mahesh");
+		System.out.println(a);
+	}
+}
