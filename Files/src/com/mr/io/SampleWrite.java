8d89c90466271abc98820799febf81f7fcda9874
Mahesh M R <maheshmr2012@gmail.com>
2018-04-15 01:10:51 +0530

    Files

---
+++ Files/src/com/mr/io/SampleWrite.java
@@ -0,0 +1,14 @@
+package com.mr.io;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class SampleWrite {
+	public static void main(String[] args) {
+		List list = Arrays.asList("Mahesh", "Darshini");
+		ArrayList al = new ArrayList(list);
+		System.out.println(al);
+
+	}
+}
